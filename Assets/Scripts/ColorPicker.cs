ae2c3776210998b34c842a6e7d30c47500c5dd2d
csdjk <1076984375@qq.com>
2019-10-08 23:02:41 +0800

    first
    Former-commit-id: 1b0625edda30ddd9c23bd62ea898dae9f6b73170

---
+++ Assets/Scripts/ColorPicker.cs
@@ -0,0 +1,89 @@
+﻿/// <summary>
+/// Author: Lele Feng 
+/// </summary>
+
+using UnityEngine;
+using System.Collections;
+
+public class ColorPicker : MonoBehaviour {
+	
+	public BoxCollider pickerCollider;
+
+	private bool m_grab;
+	private Camera m_camera;
+	private Texture2D m_screenRenderTexture;
+	private static Texture2D m_staticRectTexture;
+	private static GUIStyle m_staticRectStyle;
+
+	private static Vector3 m_pixelPosition = Vector3.zero;
+	private Color m_pickedColor = Color.white;
+
+	void Awake() {
+		// Get the Camera component
+		m_camera = GetComponent<Camera>();
+		if (m_camera == null) {
+			Debug.LogError("You need to dray this script to a camera!");
+			return;
+		}
+
+		// Attach a BoxCollider to this camera
+		// In order to receive mouse events
+		if (pickerCollider == null) {
+			pickerCollider = gameObject.AddComponent<BoxCollider>();
+			// Make sure the collider is in the camera's frustum
+			pickerCollider.center = Vector3.zero;
+			pickerCollider.center += m_camera.transform.worldToLocalMatrix.MultiplyVector(m_camera.transform.forward) * (m_camera.nearClipPlane + 0.2f);
+			pickerCollider.size = new Vector3(Screen.width, Screen.height, 0.1f);
+		}
+	}
+
+	// Draw the color we picked
+	public static void GUIDrawRect( Rect position, Color color )
+	{
+		if( m_staticRectTexture == null )
+		{
+			m_staticRectTexture = new Texture2D(1, 1);
+		}
+		
+		if( m_staticRectStyle == null )
+		{
+			m_staticRectStyle = new GUIStyle();
+		}
+		
+		m_staticRectTexture.SetPixel(0, 0, color);
+		m_staticRectTexture.Apply();
+		
+		m_staticRectStyle.normal.background = m_staticRectTexture;
+		
+		GUI.Box(position, GUIContent.none, m_staticRectStyle);
+	}
+
+	// OnPostRender is called after a camera has finished rendering the scene.
+	// This message is sent to all scripts attached to the camera.
+	// Use it to grab the screen
+	// Note: grabing is a expensive operation
+	void OnPostRender() {
+		if (m_grab) {
+			m_screenRenderTexture = new Texture2D(Screen.width, Screen.height);
+			m_screenRenderTexture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+			m_screenRenderTexture.Apply();
+			m_pickedColor = m_screenRenderTexture.GetPixel(Mathf.FloorToInt(m_pixelPosition.x), Mathf.FloorToInt(m_pixelPosition.y));
+			m_grab = false;
+		}
+	}
+	
+	void OnMouseDown() {
+		m_grab = true;
+		// Record the mouse position to pick pixel
+		m_pixelPosition = Input.mousePosition;
+	}
+
+	void OnGUI() {
+		GUI.Box(new Rect(0, 0, 120, 200), "Color Picker");
+		GUIDrawRect(new Rect(20, 30, 80, 80), m_pickedColor);
+		GUI.Label(new Rect(10, 120, 100, 20), "R: " + System.Math.Round((double)m_pickedColor.r, 4) + "\t(" + Mathf.FloorToInt(m_pickedColor.r * 255)+ ")");
+		GUI.Label(new Rect(10, 140, 100, 20), "G: " + System.Math.Round((double)m_pickedColor.g, 4) + "\t(" + Mathf.FloorToInt(m_pickedColor.g * 255)+ ")");
+		GUI.Label(new Rect(10, 160, 100, 20), "B: " + System.Math.Round((double)m_pickedColor.b, 4) + "\t(" + Mathf.FloorToInt(m_pickedColor.b * 255)+ ")");
+		GUI.Label(new Rect(10, 180, 100, 20), "A: " + System.Math.Round((double)m_pickedColor.a, 4) + "\t(" + Mathf.FloorToInt(m_pickedColor.a * 255)+ ")");
+	}
+}
--- Assets/Shader/self_demo/004_DissolveEdgeColorBlendFromPoint.shader
+++ Assets/Shader/self_demo/004_DissolveEdgeColorBlendFromPoint.shader
@@ -6,9 +6,7 @@
 		_NoiseTex("Noise", 2D) = "white" {}
 		_Threshold("Threshold", Range(0.0, 1.0)) = 0.5
 		_EdgeLength("Edge Length", Range(0.0, 0.2)) = 0.1
-		_EdgeFirstColor("Edge Color", Color) = (1,1,1,1)
-		_EdgeSecondColor("Edge Color", Color) = (1,1,1,1)
-
+		_RampTex("Ramp", 2D) = "white" {}
 		_StartPoint("Start Point", Vector) = (0, 0, 0, 0)
 		_MaxDistance("Max Distance", Float) = 0
 		_DistanceEffect("Distance Effect", Range(0.0, 1.0)) = 0.5
@@ -19,7 +17,7 @@
 
 		Pass
 		{
-			Cull Off //要渲染背面保证效果正确
+			Cull Off 
 
 			CGPROGRAM
 			#pragma vertex vert
@@ -38,7 +36,9 @@
 				float4 vertex : SV_POSITION;
 				float2 uvMainTex : TEXCOORD0;
 				float2 uvNoiseTex : TEXCOORD1;
-				float3 worldPos : TEXCOORD3;
+				float2 uvRampTex : TEXCOORD2;
+				float3 objPos : TEXCOORD3;
+				float3 objStartPos : TEXCOORD4;
 			};
 
 			sampler2D _MainTex;
@@ -47,33 +47,37 @@
 			float4 _NoiseTex_ST;
 			float _Threshold;
 			float _EdgeLength;
-			fixed4 _EdgeFirstColor;
-			fixed4 _EdgeSecondColor;
+			sampler2D _RampTex;
+			float4 _RampTex_ST;
 			float _MaxDistance;
 			float4 _StartPoint;
 			float _DistanceEffect;
-
+			
 			v2f vert (appdata v)
 			{
 				v2f o;
 				o.vertex = UnityObjectToClipPos(v.vertex);
+
 				o.uvMainTex = TRANSFORM_TEX(v.uv, _MainTex);
 				o.uvNoiseTex = TRANSFORM_TEX(v.uv, _NoiseTex);
-				o.worldPos = mul(unity_ObjectToWorld, v.vertex);
+				o.uvRampTex = TRANSFORM_TEX(v.uv, _RampTex);
+
+				o.objPos = v.vertex;
+				o.objStartPos = mul(unity_WorldToObject, _StartPoint);
+
 				return o;
 			}
 			
 			fixed4 frag (v2f i) : SV_Target
 			{
+				float dist = length(i.objPos.xyz - i.objStartPos.xyz);
+				float normalizedDist =  saturate(dist / _MaxDistance);
 
-				float dist = length(i.worldPos.xyz - _StartPoint.xyz);
-				float normalizedDist = saturate(dist / _MaxDistance);
-				//镂空
 				fixed cutout = tex2D(_NoiseTex, i.uvNoiseTex).r * (1 - _DistanceEffect) + normalizedDist * _DistanceEffect;
 				clip(cutout - _Threshold);
-				//边缘颜色
-				float degree = saturate((cutout - _Threshold) / _EdgeLength); //需要保证在[0,1]以免后面插值时颜色过亮
-				fixed4 edgeColor = lerp(_EdgeFirstColor, _EdgeSecondColor, degree);
+
+				float degree = saturate((cutout - _Threshold) / _EdgeLength);
+				fixed4 edgeColor = tex2D(_RampTex, float2(degree, degree));
 
 				fixed4 col = tex2D(_MainTex, i.uvMainTex);
 
@@ -84,3 +88,4 @@
 		}
 	}
 }
+
