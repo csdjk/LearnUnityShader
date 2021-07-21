d88163ad49ef91a943d8efd9c1fa6fb5439efa01
csdjk <1076984375@qq.com>
2021-07-21 23:12:09 +0800

    ray tracing
    Former-commit-id: 1aa234eff834d093607575b1bf79ba2ef612dea8 [formerly 2ce568f7846bf3ba6f200b51a569ef1c976847d4]
    Former-commit-id: 9ac79afb035c0630482ddccc645e60235f059968

---
+++ Assets/Scenes/RayTracing/Tutorial/AddShader.shader
@@ -0,0 +1,61 @@
+﻿Shader "Hidden/AddShader"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Cull Off 
+        ZWrite Off 
+        ZTest Always
+
+        Pass
+        {
+            Blend SrcAlpha OneMinusSrcAlpha
+
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+            #pragma multi_compile_fog
+
+
+
+            #include "UnityCG.cginc"
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                UNITY_FOG_COORDS(1)
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                UNITY_TRANSFER_FOG(o,o.vertex);
+                return o;
+            }
+            float _Sample;
+            fixed4 frag (v2f i) : SV_Target
+            {
+                return float4(tex2D(_MainTex, i.uv).rgb, 1.0f / (_Sample + 1.0f));
+            }
+            ENDCG
+        }
+    }
+}
---
+++ Assets/Scenes/RayTracing/Tutorial/part2/RayTracingMasterPart2.cs
@@ -0,0 +1,191 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class RayTracingMasterPart2 : MonoBehaviour
+{
+    public ComputeShader RayTracingShader;
+    public Light DirectionalLight;
+    public Texture SkyboxTexture;
+
+
+    [Header("Spheres")]
+    public int SphereSeed;
+    public Vector2 SphereRadius = new Vector2(3.0f, 8.0f);
+    public uint SpheresMax = 100;
+    public float SpherePlacementRadius = 100.0f;
+    private ComputeBuffer _sphereBuffer;
+
+    private Camera _camera;
+    private int _currentSamples;
+    private RenderTexture _target;
+    private uint _currentSample = 0;
+    private Material _addMaterial;
+    // part2
+    private RenderTexture _converged;
+
+    struct Sphere
+    {
+        public Vector3 position;
+        public float radius;
+        public Vector3 albedo;
+        public Vector3 specular;
+        public float smoothness;
+        public Vector3 emission;
+
+    }
+
+    private void Awake()
+    {
+        _camera = GetComponent<Camera>();
+    }
+    private void OnEnable()
+    {
+        _currentSample = 0;
+        SetUpScene();
+    }
+
+    private void OnDisable()
+    {
+        if (_sphereBuffer != null)
+            _sphereBuffer.Release();
+    }
+
+    private void Update()
+    {
+        if (transform.hasChanged)
+        {
+            _currentSample = 0;
+            transform.hasChanged = false;
+        }
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        SetShaderParameters();
+        Render(destination);
+    }
+
+    private void Render(RenderTexture destination)
+    {
+        // Make sure we have a current render target
+        InitRenderTexture();
+        int kernelHandle = RayTracingShader.FindKernel("CSMain");
+
+        uint numThreadsX, numThreadsY, numThreadsZ;
+        RayTracingShader.GetKernelThreadGroupSizes(0, out numThreadsX, out numThreadsY, out numThreadsZ);
+        int threadGroupsX = Mathf.CeilToInt(Screen.width / numThreadsX);
+        int threadGroupsY = Mathf.CeilToInt(Screen.height / numThreadsY);
+        // Set the target and dispatch the compute shader
+        RayTracingShader.SetTexture(kernelHandle, "Result", _target);
+        RayTracingShader.Dispatch(kernelHandle, threadGroupsX, threadGroupsY, 1);
+
+        // Blit the result texture to the screen
+        if (_addMaterial == null)
+            _addMaterial = new Material(Shader.Find("Hidden/AddShader"));
+        _addMaterial.SetFloat("_Sample", _currentSample);
+        Graphics.Blit(_target, _converged, _addMaterial);
+        Graphics.Blit(_converged, destination);
+        _currentSample++;
+    }
+
+    private void InitRenderTexture()
+    {
+        if (_target == null || _target.width != Screen.width || _target.height != Screen.height)
+        {
+            // Release render texture if we already have one
+            if (_target != null)
+            {
+                _target.Release();
+                _converged.Release();
+            }
+
+            // Get a render target for Ray Tracing
+            _target = new RenderTexture(Screen.width, Screen.height, 0,
+                RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+            _target.enableRandomWrite = true;
+            _target.Create();
+            _converged = new RenderTexture(Screen.width, Screen.height, 0,
+                           RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+            _converged.enableRandomWrite = true;
+            _converged.Create();
+            _currentSamples = 0;
+        }
+    }
+
+
+
+
+
+    private void SetShaderParameters()
+    {
+        RayTracingShader.SetTexture(0, "_SkyboxTexture", SkyboxTexture);
+        RayTracingShader.SetMatrix("_CameraToWorld", _camera.cameraToWorldMatrix);
+        RayTracingShader.SetMatrix("_CameraInverseProjection", _camera.projectionMatrix.inverse);
+        RayTracingShader.SetVector("_PixelOffset", new Vector2(Random.value, Random.value));
+        RayTracingShader.SetFloat("_Seed", Random.value);
+
+        Vector3 l = DirectionalLight.transform.forward;
+        RayTracingShader.SetVector("_DirectionalLight", new Vector4(l.x, l.y, l.z, DirectionalLight.intensity));
+
+        if (_sphereBuffer != null)
+            RayTracingShader.SetBuffer(0, "_Spheres", _sphereBuffer);
+    }
+
+
+    private void SetUpScene()
+    {
+        Random.InitState(SphereSeed);
+        List<Sphere> spheres = new List<Sphere>();
+
+        // Add a number of random spheres
+        for (int i = 0; i < SpheresMax; i++)
+        {
+            Sphere sphere = new Sphere();
+
+            // Radius and radius
+            sphere.radius = SphereRadius.x + Random.value * (SphereRadius.y - SphereRadius.x);
+            Vector2 randomPos = Random.insideUnitCircle * SpherePlacementRadius;
+            sphere.position = new Vector3(randomPos.x, sphere.radius, randomPos.y);
+
+            // Reject spheres that are intersecting others
+            foreach (Sphere other in spheres)
+            {
+                float minDist = sphere.radius + other.radius;
+                if (Vector3.SqrMagnitude(sphere.position - other.position) < minDist * minDist)
+                    goto SkipSphere;
+            }
+
+            // Albedo and specular color
+            Color color = Random.ColorHSV();
+            float chance = Random.value;
+            if (chance < 0.8f)
+            {
+                bool metal = chance < 0.4f;
+                sphere.albedo = metal ? Vector4.zero : new Vector4(color.r, color.g, color.b);
+                sphere.specular = metal ? new Vector4(color.r, color.g, color.b) : new Vector4(0.04f, 0.04f, 0.04f);
+                sphere.smoothness = Random.value;
+            }
+            else
+            {
+                Color emission = Random.ColorHSV(0, 1, 0, 1, 3.0f, 8.0f);
+                sphere.emission = new Vector3(emission.r, emission.g, emission.b);
+            }
+
+            // Add the sphere to the list
+            spheres.Add(sphere);
+
+        SkipSphere:
+            continue;
+        }
+
+        // Assign to compute buffer
+        if (_sphereBuffer != null)
+            _sphereBuffer.Release();
+        if (spheres.Count > 0)
+        {
+            _sphereBuffer = new ComputeBuffer(spheres.Count, 56);
+            _sphereBuffer.SetData(spheres);
+        }
+    }
+
+}
