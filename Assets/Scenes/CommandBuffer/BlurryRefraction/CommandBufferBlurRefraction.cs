70ec51c7824f774668de4d319417879c21f4865a
csdjk <1076984375@qq.com>
2021-08-27 18:37:04 +0800

    Command Buffer
    Former-commit-id: 865291139647ea956773c3aed44c7a9b50f3c945 [formerly 3b8df61c4857062960bb1436fa336c83e2cc6b41]
    Former-commit-id: 5ca0bf7b5aa1e33b7f1a8aee9f6f1d3aa677d1c1

---
+++ Assets/Scenes/CommandBuffer/BlurryRefraction/CommandBufferBlurRefraction.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using System.Collections.Generic;
+
+/// <summary>
+/// 官方Demo 
+/// </summary>
+[ExecuteInEditMode]
+public class CommandBufferBlurRefraction : MonoBehaviour
+{
+	public Shader m_BlurShader;
+	private Material m_Material;
+
+	private Camera m_Cam;
+
+	// 我们想要在渲染我们的摄像机上添加一个命令缓冲区
+	private Dictionary<Camera,CommandBuffer> m_Cameras = new Dictionary<Camera,CommandBuffer>();
+
+	// 从我们添加的所有相机中删除命令缓冲区
+	private void Cleanup()
+	{
+		foreach (var cam in m_Cameras)
+		{
+			if (cam.Key)
+			{
+				cam.Key.RemoveCommandBuffer (CameraEvent.AfterSkybox, cam.Value);
+			}
+		}
+		m_Cameras.Clear();
+		Object.DestroyImmediate (m_Material);
+	}
+
+	public void OnEnable()
+	{
+		Cleanup();
+	}
+
+	public void OnDisable()
+	{
+		Cleanup();
+	}
+
+	// 如果对象可见并且不是 UI 元素，则为每个摄像机调用 OnWillRenderObject。
+	public void OnWillRenderObject()
+	{
+		var act = gameObject.activeInHierarchy && enabled;
+		if (!act)
+		{
+			Cleanup();
+			return;
+		}
+		
+		var cam = Camera.current;
+		if (!cam)
+			return;
+
+		CommandBuffer buf = null;
+		// Did we already add the command buffer on this camera? Nothing to do then.
+		if (m_Cameras.ContainsKey(cam))
+			return;
+
+		if (!m_Material)
+		{
+			m_Material = new Material(m_BlurShader);
+			m_Material.hideFlags = HideFlags.HideAndDontSave;
+		}
+
+		buf = new CommandBuffer();
+		buf.name = "Grab screen and blur";
+		m_Cameras[cam] = buf;
+
+		// copy screen into temporary RT
+		// 复制屏幕到临时RT
+		int screenCopyID = Shader.PropertyToID("_ScreenCopyTexture");
+		buf.GetTemporaryRT (screenCopyID, -1, -1, 0, FilterMode.Bilinear);
+		buf.Blit (BuiltinRenderTextureType.CurrentActive, screenCopyID);
+		
+		// get two smaller RTs
+		int blurredID = Shader.PropertyToID("_Temp1");
+		int blurredID2 = Shader.PropertyToID("_Temp2");
+		buf.GetTemporaryRT (blurredID, -2, -2, 0, FilterMode.Bilinear);
+		buf.GetTemporaryRT (blurredID2, -2, -2, 0, FilterMode.Bilinear);
+		
+		// downsample screen copy into smaller RT, release screen RT
+		buf.Blit (screenCopyID, blurredID);
+		buf.ReleaseTemporaryRT (screenCopyID); 
+		
+		// horizontal blur
+		buf.SetGlobalVector("offsets", new Vector4(2.0f/Screen.width,0,0,0));
+		buf.Blit (blurredID, blurredID2, m_Material);
+		// vertical blur
+		buf.SetGlobalVector("offsets", new Vector4(0,2.0f/Screen.height,0,0));
+		buf.Blit (blurredID2, blurredID, m_Material);
+		// horizontal blur
+		buf.SetGlobalVector("offsets", new Vector4(4.0f/Screen.width,0,0,0));
+		buf.Blit (blurredID, blurredID2, m_Material);
+		// vertical blur
+		buf.SetGlobalVector("offsets", new Vector4(0,4.0f/Screen.height,0,0));
+		buf.Blit (blurredID2, blurredID, m_Material);
+
+		buf.SetGlobalTexture("_GrabBlurTexture", blurredID);
+
+		cam.AddCommandBuffer (CameraEvent.AfterSkybox, buf);
+	}	
+}
--- Assets/Scenes/SubsurfaceScattering/FastSSS/FastSubfaceScattering3.shader
+++ Assets/Scenes/SubsurfaceScattering/FastSSS/FastSubfaceScattering3.shader
@@ -173,7 +173,6 @@ Shader "lcl/SubsurfaceScattering/FastSubfaceScattering3" {
 				// unity_LightColor[0].rgb, unity_LightColor[1].rgb, unity_LightColor[2].rgb, unity_LightColor[3].rgb,
 				// unity_4LightAtten0,
 				// i.worldPos, normalDir);
-
 				float3 pointColor = CalculatePointLightColor(unity_4LightPosX0, unity_4LightPosY0, unity_4LightPosZ0,
 				unity_LightColor[0].rgb, unity_LightColor[1].rgb, unity_LightColor[2].rgb, unity_LightColor[3].rgb,
 				unity_4LightAtten0,
